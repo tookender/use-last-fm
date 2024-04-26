80066adb6599b678a1035b9cd66c8b0ae9f87406
ender <68495763+tookender@users.noreply.github.com>
2024-04-26 21:55:51 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,15 @@ import useSWR from 'swr';
 /**
  * Use Last.fm
  * @param username The username of the last.fm user to track
- * @param token Your API token
  * @param interval Optional, this is the interval between each request
  * @param imageSize The size of the image
  */
 export function useLastFM(
   username: string,
-  token: string,
   interval: number = 15 * 1000,
   imageSize: TrackImage['size'] = 'extralarge',
 ): State {
-  const endpoint = `//ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${token}&format=json&limit=1`;
+  const endpoint = `//ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${process.env.LASTFM_KEY}&format=json&limit=1`;
 
   const { data: track = null, error } = useSWR<LastFMResponseBody, Error>(
     endpoint,
